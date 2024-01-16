88c1a5efa021f3c4c16c57c0143e5882d49520ac
yadermena <yadermena7@gmail.com>
2024-01-15 22:05:27 -0600

    Config

---
+++ src/app/show-task/show-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-task.component.html',
+  styleUrl: './show-task.component.css'
+})
+export class ShowTaskComponent {
+
+}
